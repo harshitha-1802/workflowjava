1fb2c67d7285c4d356da85e13f9f2e5df1a61e34
harshitha-1802 <23211a6709@bvrit.ac.in>
2025-06-28 12:36:53 +0530

    Update add.java

--- add.java
+++ add.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 
-public class Add {
+public class add {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int num1 = sc.nextInt();
     int num2 = sc.nextInt();
     int sum = num1 + num2;
     System.out.println("The sum is : " + sum);
-    sc.close();  // Good practice to close the scanner
+    sc.close();
   }
 }
