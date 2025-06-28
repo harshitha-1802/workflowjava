ec7e38f1dc56b70bd2abada3a3a3ae3f2af99633
harshitha-1802 <23211a6709@bvrit.ac.in>
2025-06-28 12:34:04 +0530

    Update add.java

--- add.java
+++ add.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class AddNumbers(){
+public class add(){
   public static void main(String[] args){
     Scanner sc=new Scanner(System.in);
     int num1=sc.nextInt();
