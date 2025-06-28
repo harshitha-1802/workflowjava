1e084d03f43e30303d4f2dc9c78eb6a0e400efa6
harshitha-1802 <23211a6709@bvrit.ac.in>
2025-06-28 12:24:22 +0530

    Update add.java

--- add.java
+++ add.java
@@ -1,1 +1,10 @@
-
+import java.util.Scanner;
+public class AddNumbers(){
+  public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    int num1=sc.nextInt();
+     int num2=sc.nextInt();
+    int sum=n1+n2;
+    System.out.println("The sum is : " +sum);
+  }
+}
